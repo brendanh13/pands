916c7325dda3d359fe6dd56d9cf567edd414a98a
brendanh13 <brendanheeney@gmail.com>
2022-02-13 10:26:59 +0000

    repeat week 2 lecture

--- code/week02/addOne.py
+++ code/week02/addOne.py
@@ -1,7 +1,8 @@
 #read in a number and add one
 #Author: Brendan
 
-
+#we must convert the string to an integer by using INT 
 number = int(input("Please enter a number"))
 newNumber = number + 1
 print ('{} plus one is {}'.format(number, newNumber))
+
---
+++ code/week02/helloName.py
@@ -0,0 +1,27 @@
+# Uses a variable to greet
+# Author: Brendan Heeney
+
+name = "Brendan"
+print (name)
+
+
+
+name = "Coder"
+print ('Hello ' + name)
+
+
+# this won't work
+
+#age = 36
+#print ('Your age is' + age)
+
+#two alternatives
+
+age = 36
+print ('Your age is ' + str(age))
+
+print ('Your age is {}'.format(age))
+
+
+print ('Hello {}   \tYour age is {}'.format(name,age))
+
